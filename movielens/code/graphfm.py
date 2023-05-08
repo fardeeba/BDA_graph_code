0819815fb7b36f5c67d4493c2a102d5309bf63bc
fardeeba <Farah.Deeba@afiniti.com>
2023-05-09 02:18:06 +0500

    vaiable scope fix

--- movielens/code/graphfm.py
+++ movielens/code/graphfm.py
@@ -56,16 +56,16 @@ def gat_attention(queries,
 
     # Linear projections
     with tf.compat.v1.variable_scope("att_blocks%d" % block):
-        if field > 0: tf.tf.compat.v1.get_variable_scope().reuse_variables()
+        if field > 0: tf.compat.v1.get_variable_scope().reuse_variables()
         A = tf.compat.v1.layers.dense(values, num_heads, activation=tf.nn.relu,
                         name='att_b%d_f%d'% (block, 0))
     with tf.compat.v1.variable_scope("w_blocks%d" % block):
-        # if field > 0: tf.tf.compat.v1.get_variable_scope().reuse_variables()
+        # if field > 0: tf.compat.v1.get_variable_scope().reuse_variables()
         H = tf.compat.v1.layers.dense(values, num_units, activation=None, use_bias=False, name='w_b%d_f%d'% (block, field))
         # A = tf.compat.v1.layers.dense(H, num_heads, activation=tf.nn.relu, name='att_b%d_f%d'%(block, field))
     if has_residual:
         with tf.compat.v1.variable_scope("res_blocks%d" % block):
-            # if field > 0: tf.tf.compat.v1.get_variable_scope().reuse_variables()
+            # if field > 0: tf.compat.v1.get_variable_scope().reuse_variables()
             Q_res = tf.compat.v1.layers.dense(queries, num_units, activation=tf.nn.relu, name='res_b%d_f%d'% (block, field)) # [batch_size, 1, num_units]
 
     # Split and concat
@@ -74,7 +74,7 @@ def gat_attention(queries,
 
     # keep the top k nodes
     with tf.compat.v1.variable_scope("gsl_blocks%d" % block):
-        if field > 0: tf.tf.compat.v1.get_variable_scope().reuse_variables()
+        if field > 0: tf.compat.v1.get_variable_scope().reuse_variables()
         S = tf.compat.v1.layers.dense(values, 16, activation=tf.nn.relu, name='gsl_1_b%d_f%d' % (block, 0))
         S = tf.compat.v1.layers.dense(S, 1, activation=tf.nn.sigmoid,
                             name='gsl_2_b%d_f%d' % (block, 0))  # [batch_size, field_size, 1]
