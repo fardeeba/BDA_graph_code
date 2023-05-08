1b53a8ef032757b9bbf64a015def818dd802d1a1
fardeeba <Farah.Deeba@afiniti.com>
2023-05-09 01:41:04 +0500

    dropout arg bug fix

--- movielens/code/graphfm.py
+++ movielens/code/graphfm.py
@@ -92,7 +92,7 @@ def gat_attention(queries,
     weights = tf.nn.softmax(A_, axis=1)
 
     # Dropouts
-    weights = tf.compat.v1.layers.Dropout(weights, rate=1-dropout_keep_prob,
+    weights = tf.compat.v1.layers.Dropout(weights, rate=(1-dropout_keep_prob),
                                         training=tf.convert_to_tensor(is_training))
 
     # Weighted sum
