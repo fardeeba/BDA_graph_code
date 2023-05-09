111181cf1bae7c08635137a237a63d7c9b7e7f2d
fardeeba <Farah.Deeba@afiniti.com>
2023-05-09 22:59:07 +0500

    path update 3

--- data/Dataprocess/Kfold_split/config.py
+++ data/Dataprocess/Kfold_split/config.py
@@ -1,4 +1,4 @@
-DATA_PATH = '/mnt/dev_sdb1/lizekun/Wiki/'
+DATA_PATH = '/kaggle/working/dataset/criteo/'
 TRAIN_I = DATA_PATH + 'train_i.txt'
 TRAIN_X = DATA_PATH + 'train_x.txt'
 TRAIN_Y = DATA_PATH + 'train_y.txt'
