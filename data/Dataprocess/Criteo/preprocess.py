3ad4cfa0630fd8685bf14869d4948716562dd743
fardeeba <Farah.Deeba@afiniti.com>
2023-05-10 01:26:06 +0500

    preprocess add new

--- data/Dataprocess/Criteo/preprocess.py
+++ data/Dataprocess/Criteo/preprocess.py
@@ -21,7 +21,7 @@ cnt_train = 0
 index = [1] * 26
 for line in f1:
     cnt_train +=1
-    if cnt_train % 100000 ==0:
+    if cnt_train % 1000 ==0:
         print('now train cnt : %d\n' % cnt_train)
     #if cnt_train > limits:
     #	break
