a316dc8f9c94f47540a70c4514c6a922c1b22a94
fardeeba <Farah.Deeba@afiniti.com>
2023-05-09 22:51:04 +0500

    path update 2

--- data/Dataprocess/Criteo/config.py
+++ data/Dataprocess/Criteo/config.py
@@ -1,2 +1,2 @@
-DATA_PATH = '/kaggle/input/criteo-small/'
-SOURCE_DATA = '/kaggle/input/criteo-small/train_1m.txt'
+DATA_PATH = '/kaggle/working/dataset/criteo/'
+SOURCE_DATA = '/kaggle/working/dataset/criteo/train.txt'
